bfbb76074de3d31fc2a2b500a7ceb3021d3d5933
Scrappycow <scrivon@gmail.com>
2023-05-12 05:43:04 -0700

    advanced tasks

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,6 +4,6 @@ if __name__ == "__main__":
     import sys
 
     total = 0
-    for t in range(len(sys.arg) - 1):
-        total += int(sys.arg[t + 1])
+    for t in range(len(sys.argv) - 1):
+        total += int(sys.argv[t + 1])
     print("{}".format(total))
