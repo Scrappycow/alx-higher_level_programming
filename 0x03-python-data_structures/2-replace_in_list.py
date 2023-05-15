ce2b7d708909a03cbcdb6724271ab503d7a8462e
Scrappycow <scrivon@gmail.com>
2023-05-15 15:05:28 -0700

    more tasks

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 
 def replace_in_list(my_list, idx, element):
-    if idx >= 0 and idx < len(my_list):
-        my list[idx] = element
-    return (my_list)
+    len_of_list = len(my_list)
+    if idx < 0:
+        return (my_list)
+    elif idx > len_of_list - 1:
+        return (my_list)
+    else:
+        my_list[idx] = element
+        return (my_list)
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-    if idx < 0 or idx > (len(my_list) -1):
-        return (my_list)
-
-    copy = [y for y in my_list]
-    copy[idx] = element
-    return (copy)
+    x = len(my_list)
+    copy_list = my_list.copy()
+    if idx < 0:
+        return copy_list
+    elif idx > n - 1:
+        return copy_list
+    else:
+        copy_list[idx] = element
+        return copy_list
