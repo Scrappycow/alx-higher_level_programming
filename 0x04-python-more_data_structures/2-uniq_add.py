2804480691dfa3269f3b082ae10862fefa5dcbfb
Scrappycow <scrivon@gmail.com>
2023-05-17 12:21:54 -0700

    unique

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 
 def uniq_add(my_list=[]):
+    newest_list = set(my_list)
     result = 0
-    for x in set(my_list):
-        result += x
+    for t in newest_list:
+        result += t
         return (result)
