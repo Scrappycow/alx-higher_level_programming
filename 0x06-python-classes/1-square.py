8e2bce6e1c83bde3ba7bac8301124e56fefa1162
Scrappycow <scrivon@gmail.com>
2023-05-30 11:10:33 -0700

    1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+"""define a class Square"""
 
 
 class Square:
+    """a square"""
+
     def __init__(self, size):
+        """initialize a new square
+
+        Args:
+        size (int): size of a new square
+        """
         self.__size = size
