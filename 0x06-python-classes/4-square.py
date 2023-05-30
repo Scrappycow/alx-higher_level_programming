69e454b436c32922fb885112f1345407a12f6038
Scrappycow <scrivon@gmail.com>
2023-05-30 10:44:53 -0700

    tasks

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+
+class Square:
+
+    def __init__(self, size=0):
+        self.size = size
+
+    def area(self):
+
+        return (Self.__size) ** 2
+
+    def size(self):
+
+        return self.__size
+
+    def soze(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        else:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            else:
+                self.__size = value
