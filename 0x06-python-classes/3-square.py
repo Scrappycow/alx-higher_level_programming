9ec44155963b1d077c35ac09102d4fe7bec17800
Scrappycow <scrivon@gmail.com>
2023-05-30 11:15:43 -0700

    3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
+"""define a class Square"""
 
 
 class Square:
+    """represent a square"""
+
     def __init__(self, size=0):
+        """Initialize a square
+
+        Args:
+        size (int): new square size
+        """
         if type(size) is not int:
             raise TypeError("size must be an integer")
         else:
