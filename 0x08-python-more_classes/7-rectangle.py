784b70226d7b112ddda079e7a6bc4f22487e3c54
Scrappycow <scrivon@gmail.com>
2023-06-07 22:58:46 -0700

    task 7 corrs

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -54,7 +54,7 @@ class Rectangle:
     def __str__(self):
         string = ""
         if self.__width != 0 and self.__height != 0:
-            string += "\n".join("#" * self.__width
+            string += "\n".join(str(self.print_symbol) * self.__width
                                 for i in range(self.__height))
         return string
 
