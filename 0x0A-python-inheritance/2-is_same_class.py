9efafa6da2f2959d54d9dc22785650d69016fbda
Scrappycow <scrivon@gmail.com>
2023-06-13 11:21:40 -0700

    task 2 more corrs

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""True = the object is exactly an instance of specified class"""
+"""
+True = the object is exactly an instance of specified class
+"""
 
 def is_same_class(obj, a_class):
     """True = the object is exactly an instance of specified class"""
