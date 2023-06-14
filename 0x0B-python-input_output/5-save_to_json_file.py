1a6475020831bb308c191d7cf122a30bf3b70811
Scrappycow <scrivon@gmail.com>
2023-06-14 01:27:54 -0700

    corrs l

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,4 +7,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """function that writes an Object to a text file, using a JSON rep"""
     with open(filename, 'w', encoding="utf-8") as file:
-        json.dump(my_obj, file()
+        json.dump(my_obj, file)
