d4d45eb03a35e2c38b30d24145db934f478c74a9
Scrappycow <scrivon@gmail.com>
2023-06-14 01:07:20 -0700

    tasks 1-2

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""reads a text file (UTF8) and prints it to stdout"""
+
+
+def read_file(filename=""):
+    """prints file in UTF8"""
+    with open(filename, 'r', encoding="utf-8) as file:
+        print(file.read(), end"")
---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""appends string at the end of a text file and returns num of chars added"""
+
+
+def append_write(filename="", text=""):
+    """appends string at the end of a file and returns num of chars added"""
+    with open(filename, 'a', encoding="utf-8") as file:
+        file.append(text)
+        return file
