b44455045725bdae0179eb42d8c428bcd11003a9
Scrappycow <scrivon@gmail.com>
2023-06-14 01:08:45 -0700

    kk

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 
 def read_file(filename=""):
     """prints file in UTF8"""
-    with open(filename, 'r', encoding="utf-8) as file:
+    with open(filename, 'r', encoding="utf-8") as file:
         print(file.read(), end"")
