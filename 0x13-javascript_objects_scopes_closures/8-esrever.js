66776a033977ba52ad701d65930dcab44304133f
Scrappycow <scrivon@gmail.com>
2023-09-30 04:27:00 -0700

    man tasks

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-// function that returns the reversed version of a list 
+// function that returns the reversed version of a list
 
 exports.esrever = function (list) {
   const revList = [];
