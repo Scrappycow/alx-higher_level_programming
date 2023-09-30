1a732d3efab25ebb8ba4e676fa28a9262b87fb24
Scrappycow <scrivon@gmail.com>
2023-09-30 08:09:21 -0700

    ddd

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,22 +2,4 @@
 """contains the function called find_peak"""
 
 
-def find_peak(list_of_integers):
-    """finds a peak in the list of unsorted integers"""
-     if list_of_integers == []:
-        return None
-
-    size = len(list_of_integers)
-    if size == 1:
-        return list_of_integers[0]
-    elif size == 2:
-        return max(list_of_integers)
-
-    middle = int(size / 2)
-    peak = list_of_integers[middle]
-    if peak > list_of_integers[middle - 1] and peak > list_of_integers[middle + 1]:
-        return peak
-    elif peak < list_of_integers[middle - 1]:
-        return find_peak(list_of_integers[:middle])
-    else:
-        return find_peak(list_of_integers[middle + 1:])
+ef find_peak(list_of_integers):
