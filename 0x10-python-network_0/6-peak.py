b330169c6a5b2c3dafec02d6dc6043a45aff00cf
Scrappycow <scrivon@gmail.com>
2023-10-01 10:28:43 -0700

    corss x10

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -2,4 +2,15 @@
 """contains the function called find_peak"""
 
 
-ef find_peak(list_of_integers):
+def find_peak(list_of_integers):
+    """finds a peak in a list of unsorted integers"""
+    li = list_of_integers
+    ll = len(li)
+    if ll == 0:
+        return
+    m = ll // 2
+    if (m == ll - 1 or li[m] >= li[m + 1]) and (m == 0 or li[m] >= li[m - 1]):
+        return li[m]
+    if m != ll - 1 and li[m + 1] > li[m]:
+        return find_peak(li[m + 1:])
+    return find_peak(li[:m])
