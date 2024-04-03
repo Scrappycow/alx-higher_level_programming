2557a97a35ddf07f83bde7cb68b67dd771d6fec5
Scrappycow <116637627+Scrappycow@users.noreply.github.com>
2024-04-03 21:42:50 +0300

    tasks

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+function getDataFrom (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (err, _res, body) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(body);
+      }
+    });
+  });
+}
+
